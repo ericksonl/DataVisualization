6e948de883d68711266a75ecd2777c2d752f555d
Liam <liamerickson20@gmail.com>
2024-01-21 21:44:41 -0800

    Adding legend

--- Treemap/main.js
+++ Treemap/main.js
@@ -1,4 +1,4 @@
-const DATASET = {
+const DATASETS = {
     videoGames: {
         TITLE: 'Video Game Sales',
         DESCRIPTION: 'Top 100 most sold video games grouped by platform',
@@ -16,50 +16,26 @@ const DATASET = {
     }
 }
 
-const defaultDataset = DATASET.movies
+const defaultDataset = DATASETS.videoGames
+
+const width = 1300
+const height = 665
+
+const legendW = 350
+const legendH = 320
 
 var dataset
-const width = 1000
-const height = 600
-var svg
 
+var svg
+var colorsScale
+var legend
 var colorArray = []
 
-for (i = 0; i < 1; i += 0.05) {
-    let color = d3.interpolateRainbow(i)
-    colorArray.push(color)
-}
-
-console.log(colorArray)
-
-//create threshold for fill values
-var colors = d3.scaleOrdinal()
-    .domain([1, 20])
-    .range(colorArray)
-
-console.log(colors[0])
-
-
 document.addEventListener('DOMContentLoaded', function () {
-    var div = d3.select('.heading')
-        .append('div')
-        .attr('id', 'buttons')
-
-    div.selectAll('button')
-        .data(Object.keys(DATASET)) //.data expects an array, Object.keys(DATASET) returns an array of keys from the DATASET object
-        .enter()
-        .append('button')
-        .attr('class', 'buttons')
-        .text((index) => DATASET[index].TITLE)
-        .on('click', (d) => {
-            dataset = DATASET[d]
-            loadData(DATASET[d].URL)
-            changeData(DATASET[d].TITLE, DATASET[d].DESCRIPTION)
-        })
 
     initCall()
     loadData(defaultDataset.URL)
-    changeData(defaultDataset.TITLE, defaultDataset.DESCRIPTION)
+    updateData(defaultDataset.TITLE, defaultDataset.DESCRIPTION)
 })
 
 function loadData(url) {
@@ -77,32 +53,70 @@ function loadData(url) {
 }
 
 function initCall() {
-    d3.select('body')
+
+    for (i = 0; i < 1; i += 0.05) {
+        let color = d3.interpolateRainbow(i)
+        colorArray.push(color)
+    }
+
+    colorsScale = d3.scaleOrdinal()
+        .domain([1, 20])
+        .range(colorArray)
+
+    d3.select('#heading')
         .append('h1')
         .attr('id', 'title')
         .text(defaultDataset.TITLE)
 
-    d3.select('body')
+    d3.select('#heading')
         .append('h3')
         .attr('id', 'description')
         .text(defaultDataset.DESCRIPTION)
 
-    svg = d3.select('body')
-        .append('svg')
+    var div = d3.select('#heading')
+        .append('div')
+        .attr('id', 'buttons')
+
+    div.selectAll('button')
+        .data(Object.keys(DATASETS)) //.data expects an array, Object.keys(DATASETS) returns an array of keys from the DATASETS object
+        .enter()
+        .append('button')
+        .attr('class', 'buttons')
+        .text((index) => DATASETS[index].TITLE)
+        .on('click', (index) => {
+            //set dataset to DATASET[index]
+            dataset = DATASETS[index]
+            //load new dataset
+            loadData(DATASETS[index].URL)
+            //update HTML elements
+            updateData(DATASETS[index].TITLE, DATASETS[index].DESCRIPTION)
+        })
+
+    svg = d3.select('#main-content')
+        .append('div')
         .attr('id', 'canvas')
+        .append('svg')
+        .attr('id', 'svg')
         .attr('width', width)
         .attr('height', height)
+
+    legend = d3.select('#main-content')
+        .append('div')
+        .attr('id', 'legend')
+        .append('svg')
+        .attr('width', legendW)
+        .attr('height', legendH)
 }
 
 
 function drawDiagram() {
     //setup dataset hierarchy
-    let hierarchy = d3.hierarchy(dataset, (node) =>
+    const hierarchy = d3.hierarchy(dataset, (node) =>
         node['children'])
         .sum((node) => node['value'])
         .sort((node1, node2) => node2['value'] - node1['value']) //returns integer. If positive, node2 comes before node1. If negative, node1 before node2
 
-    let createTreeMap = d3.treemap()
+    const createTreeMap = d3.treemap()
         .size([width, height])
 
     createTreeMap(hierarchy)//gives all leaf nodes coordinates
@@ -119,30 +133,67 @@ function drawDiagram() {
     //create rectangles
     group.append('rect')
         .attr('class', 'tile')
-        .style('fill', (d, i) => colors(d.data.category))
+        .style('fill', (d, i) => colorsScale(d.data.category))
         .attr('data-name', (d) => d.data.name)
         .attr('data-category', (d) => d.data.category)
         .attr('data-value', (d) => d.data.value)
         .attr('width', (d) => d.x1 - d.x0)
         .attr('height', (d) => d.y1 - d.y0)
 
-    group.append("text")
-        .attr('class', 'tile-text')
-        .selectAll("tspan")
-        .data(function (d) { return d.data.name.split(/(?=[A-Z][^A-Z])/g); })
-        .enter().append("tspan")
-        .attr("x", 4)
-        .attr("y", function (d, i) { return 13 + i * 10; })
-        .text(function (d) { return d; });
+    group.append('text')
+        .text((d) => d.data.name)
+        .attr('x', 5)
+        .attr('y', 20)
+
+
+    //create an array of categories
+    const categories = Array.from(new Set(dataset.children.flatMap(child => child.children.map(innerChild => innerChild.category))));
+
+    dataset.children.forEach(element => {
+        console.log("child", element.children)
+    });
+
+    //Output: what (18) ['2600', 'Wii', 'NES', 'GB', 'DS', 'X360', 'PS3', 'PS2', 'SNES', 'GBA', 'PS4', '3DS', 'N64', 'PS', 'XB', 'PC', 'PSP', 'XOne']
+
+    var legendGroup = legend.selectAll('g')
+        .data(categories) // Example data, you can replace it with your own
+        .enter()
+        .append('g')
+        .attr('transform', (d, i) => {
+            let xVal = i < 9 ? 0 : 200
+            let yVal = i < 9 ? i * 35 : (i * 35) - 350
+            return (`translate(${xVal}, ${yVal})`)
+        })
+        .append('rect')
+        .attr('width', 35) // Set the width of the rectangle
+        .attr('height', 35) // Set the height of the rectangle
+        .attr('class', 'legend-item')
+        .style('stroke', 'black')
+        .style('fill', d => colorsScale(d));
+
+    //Add text labels for each category in the legend
+    legend.selectAll('text')
+        .data(categories)
+        .enter()
+        .append('text', (d, i) => categories[i])
+        .text(d => `-${d}`)
+        .attr('x', (d, i) => i < 9 ? 50 : 250)
+        .attr('y', (d, i) => i < 9 ? (i * 35) + 25 : (i * 35) - 325)
+        .attr('class', 'legend-text')
 
     console.log(hierarchy.leaves())
 }
 
 
-function changeData(dataTitle, dataDescr) {
+function updateData(dataTitle, dataDescr) {
+    //remove current data
+    d3.selectAll('svg > *').remove();
+
+    //Change page title
     d3.select('#title')
         .text(dataTitle)
 
+    //change page description
     d3.select('#description')
         .text(dataDescr)
 }
